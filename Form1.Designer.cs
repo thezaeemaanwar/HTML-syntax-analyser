f5688fb4ddba0245028a3a69401ef9c993a2a1ad
Zaeema Anwar <thezaeemaanwar@gmail.com>
2020-11-27 23:28:16 +0500

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace HTMLSyntaxAnalyser
+{
+    partial class HTMLSyntaxAnalyser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.processToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.htmlValidity = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkTagsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.processToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // processToolStripMenuItem
+            // 
+            this.processToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.checkTagsToolStripMenuItem});
+            this.processToolStripMenuItem.Name = "processToolStripMenuItem";
+            this.processToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.processToolStripMenuItem.Text = "Process";
+            this.processToolStripMenuItem.Click += new System.EventHandler(this.processToolStripMenuItem_Click);
+            // 
+            // htmlValidity
+            // 
+            this.htmlValidity.AutoSize = true;
+            this.htmlValidity.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.htmlValidity.Location = new System.Drawing.Point(267, 45);
+            this.htmlValidity.Name = "htmlValidity";
+            this.htmlValidity.Size = new System.Drawing.Size(301, 24);
+            this.htmlValidity.TabIndex = 2;
+            this.htmlValidity.Text = "Select HTML file to view the validity";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(26, 82);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(744, 394);
+            this.listBox1.TabIndex = 3;
+            // 
+            // checkTagsToolStripMenuItem
+            // 
+            this.checkTagsToolStripMenuItem.Name = "checkTagsToolStripMenuItem";
+            this.checkTagsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.checkTagsToolStripMenuItem.Text = "Check Tags";
+            this.checkTagsToolStripMenuItem.Click += new System.EventHandler(this.checkTagsToolStripMenuItem_Click);
+            // 
+            // HTMLSyntaxAnalyser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 561);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.htmlValidity);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "HTMLSyntaxAnalyser";
+            this.Text = "HTML Syntax Analyser";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem processToolStripMenuItem;
+        private System.Windows.Forms.Label htmlValidity;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ToolStripMenuItem checkTagsToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+
+namespace HTMLSyntaxAnalyser
+{
+    public partial class HTMLSyntaxAnalyser : Form
+    {
+        string html = "";
+        string tags = "";
+        bool fileSelected = false;
+
+        public HTMLSyntaxAnalyser()
+        {
+            InitializeComponent();
+            processToolStripMenuItem.Visible = false;
+            listBox1.Visible = false;
+        }
+        private void ValidateHTMLTags(string html, List<string>outPut)
+        {
+            Stack<string> myStack = new Stack<string>();
+            bool isValidHTML = true;
+            int startInd = 0;
+            int endInd = 0;
+            while (html.Length != 0)
+            {
+                startInd = html.IndexOf('<');
+                endInd = html.IndexOf('>');
+                string sub = html.Substring(startInd, (endInd - startInd + 1));
+                if (sub.Contains("/>"))
+                {
+                    if (sub.Contains(" "))
+                    {
+                        outPut.Add(tags + "Encountered a self closing tag : " + sub.Substring(0, sub.IndexOf(' ')) + "/>");
+                    }
+                    else
+                    {
+                        outPut.Add(tags + "Encountered a self closing tag : " + sub);
+                    }
+                }
+                else if (sub == "<br>" || sub == "</br>" || sub == "<hr>" || sub.Contains("<meta") ||
+                    sub.Contains("<!") || sub.Contains("<img") || sub.Contains("<link") ||
+                    sub.Contains("<frame") || sub.Contains("<base") || sub.Contains("<col") ||
+                    sub.Contains("<input") || sub.Contains("<area") || sub.Contains("<param") ||
+                    sub.Contains("<isindex"))
+                {
+                    // Empty tags
+                    if (sub.Contains(" "))
+                    {
+                        outPut.Add(tags + "Encountered an empty tag : " + sub.Substring(0, sub.IndexOf(' ')) + "/>");
+                    }
+                    else
+                    {
+                        outPut.Add(tags + "Encountered an empty tag : " + sub);
+                    }
+                }
+                else if (sub.Contains("</"))
+                {
+                    string closeTag = sub.Remove(1, 1).ToLower();
+                    string openTag = myStack.Pop().ToString();
+                    if (openTag == closeTag)
+                    {
+                        if (tags.Length > 1)
+                        {
+                            tags = tags.Substring(0, tags.Length - 1);
+                        }
+                        else
+                        {
+                            tags = "";
+                        }
+                        outPut.Add(tags + "Encountered a closing tag : " + sub);
+                    }
+                    else
+                    {
+                        isValidHTML = false;
+                        myStack.Push(openTag.ToLower());
+                        outPut.Add("Wrong closing" + closeTag + ", Opening : "+openTag);
+                    }
+                }
+                else if (sub.Contains("<!--"))
+                {
+                    if (sub.Contains("-->"))
+                    {
+                        //It is comment so do nothing.
+                        outPut.Add(tags + "Encountered a Comment");
+                    }
+                    else
+                    {
+                        outPut.Add("wrong Comment");
+                        isValidHTML = false;
+                    }
+                }
+                
+                else
+                {
+                    if (sub.Contains(" "))
+                    {
+                        string opening = sub.Substring(0, sub.IndexOf(' ')) + ">";
+                        outPut.Add(tags+"Encountered an opening tag : " + opening);
+                        myStack.Push(opening.ToLower());
+                        tags += "\t";
+                    }
+                    else
+                    {
+                        outPut.Add(tags+"Encountered an opening tag : " + sub);
+                        myStack.Push(sub.ToLower());
+                        tags += "\t";
+                    }
+                }
+                html = html.Substring(endInd + 1);
+            }
+            if (myStack.Count != 0)
+            {
+                outPut.Add("Stack not empty"+myStack.Count);
+                isValidHTML = false;
+            }
+            if (isValidHTML)
+            {
+                htmlValidity.Text = "All HTML Tags are balanced";
+            }
+            else
+            {
+                htmlValidity.Text = "All HTML Tags are not balanced";
+
+            }
+
+        }
+        private void ChooseFileButton_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string filePath = "";
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.AddExtension = true;
+            openFileDialog1.Multiselect = false;
+            openFileDialog1.Filter = "HTML files (*.html)|*.html";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                filePath = openFileDialog1.FileName;
+                MessageBox.Show(filePath);
+                html = File.ReadAllText(filePath);
+                fileSelected = true;
+                processToolStripMenuItem.Visible = true;
+            }
+            htmlValidity.Text = "Press Process to continue Validity.";
+        }
+
+        private void processToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void checkTagsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (fileSelected)
+            {
+                tags = "";
+                List<string> outPut = new List<string>();
+                ValidateHTMLTags(html, outPut);
+                this.listBox1.DataSource = outPut;
+                listBox1.Visible = true;
+
+            }
+            else
+            {
+                MessageBox.Show("Select a file first");
+            }
+        }
+    }
+}
